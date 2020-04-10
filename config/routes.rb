c16fd5730038b3737a1420d96596b36fbc620675
Ramu <ramu@coppernine.com>
2020-04-10 17:08:16 +0530

    all setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'invoices#index'
+  root to: 'statistics#index'
 
   resources :invoices, only: [:index, :show]
-  
+
   resources :statistics do
     delete :delete_excel_attachment
     get    :result_excel_analysis
