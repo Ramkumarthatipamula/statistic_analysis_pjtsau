ad16ee0a55fb284a7b811eb543ee69f52715f5e6
Ramu <ramkumar.ramu1@gmail.com>
2020-07-13 19:28:13 +0530

    added sigout devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
+
+  # get '/users/sign_out' => 'devise/sessions#destroy'
   resources :application_forms
   root to: 'application_forms#index'
 
